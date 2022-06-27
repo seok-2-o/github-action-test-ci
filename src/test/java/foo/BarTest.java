ab6a4ff4469422f30de065fc96198a3cffd8b37e
seok2 <seogi89@gmail.com>
2022-06-27 20:56:00 +0900

    <test> 성공 테스트 1 / 실패테스트 1

---
+++ src/test/java/foo/BarTest.java
@@ -0,0 +1,21 @@
+package foo;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class BarTest {
+
+    @Test
+    @DisplayName("성공하는 테스트")
+    void foo() {
+        assertThat(true).isTrue();
+    }
+
+    @Test
+    @DisplayName("실패하는 테스트")
+    void bar() {
+        assertThat(true).isTrue();
+    }
+}
